aef2a2aa0401697f6748039eba428a9e4d357b4e
Anita David <anitafunmilayo@gmail.com>
2025-06-04 21:44:45 +0100

    Updated image file paths

--- public/Assets/data.jsx
+++ public/Assets/data.jsx
@@ -1,32 +1,32 @@
 export const pictures = [
   {
     id: "text1",
-    src: "../public/Assets/images/Vals-Thorens.png",
+    src: "/Assets/images/Vals-Thorens.png",
     alt: "Val Thorens",
   },
   {
     id: "text2",
-    src: "../public/Assets/images/Restaurant-Terrace.png",
+    src: "/Assets/images/Restaurant-Terrace.png",
     alt: "Restaurant Terrace",
   },
   {
     id: "text3",
-    src: "../public/Assets/images/An-outdoor-cafe.png",
+    src: "/Assets/images/An-outdoor-cafe.png",
     alt: "An outdoor cafe",
   },
   {
     id: "text4",
-    src: "../public/Assets/images/A-very-long-bridge-over-the-forest.png",
+    src: "/Assets/images/A-very-long-bridge-over-the-forest.png",
     alt: "A very long bridge over the forest...",
   },
   {
     id: "text5",
-    src: "../public/Assets/images/Tunnel-with-the-morning-light.png",
+    src: "/Assets/images/Tunnel-with-the-morning-light.png",
     alt: "Tunnel with the morning light",
   },
   {
     id: "text6",
-    src: "../public/Assets/images/Mountain-house.png",
+    src: "/Assets/images/Mountain-house.png",
     alt: "Mountain House",
   },
 ];
